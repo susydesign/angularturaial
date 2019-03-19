ef6e4a87b7c53155bc376b65c66bbbac63d05691
susydesign <tarnavz@gmail.com>
2019-03-19 15:26:53 +0100

    4. Service dolgozik
    Signed-off-by: susydesign <tarnavz@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,24 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
+
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
