3c0211287865ed53e180b853140d2cb1ff96e46d
susydesign <tarnavz@gmail.com>
2019-03-19 12:54:34 +0100

    2. Lecke Listazas

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Topa' },
+  { id: 12, name: 'Krisz' },
+  { id: 13, name: 'Andi' },
+  { id: 14, name: 'Brigi' },
+  { id: 15, name: 'Zsozsó' },
+  { id: 16, name: 'Karesz' },
+  { id: 17, name: 'Frenk' },
+  { id: 18, name: 'Arlet' },
+  { id: 19, name: 'Tercsi' },
+  { id: 20, name: 'Fercsi' }
+];
